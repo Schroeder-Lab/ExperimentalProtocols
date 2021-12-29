7e610b90b717e0322853e455d3818c2659f8bb7d
Maria Cozan <maria.cozan@gmail.com>
2021-12-29 17:30:24 +0000

    fixed wrong entry

--- Bonvision/Maria/monitor_calibration/20211229_full_monitor_calibration.py
+++ Bonvision/Maria/monitor_calibration/20211229_full_monitor_calibration.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 29 08:50:55 2021
+# # -*- coding: utf-8 -*-
+# """
+# Created on Wed Dec 29 08:50:55 2021
 
-@author: maria
-"""
+# @author: maria
+# """
 
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import numpy as np
 #loading file
 #file_path= "C://Users//maria//Desktop//PhD//Code//Calibration_blue3"
 file_path_b= "C://MyPrograms//Bonvision//output//Calibration_blue9"
@@ -25,3 +26,22 @@ def NormalizeData(data):
 norm_array_b=  NormalizeData(array_b)
 norm_array_g=  NormalizeData(array_g)
 norm_array_r=  NormalizeData(array_r)
+
+norm_red=pd.DataFrame(norm_array_r, dtype='float64')
+norm_green= pd.DataFrame(norm_array_g, dtype='float64')
+norm_blue=pd.DataFrame(norm_array_b, dtype='float64')
+
+norm_red.to_csv('C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//Maria//monitor_calibration//normalised_red.csv')
+norm_green.to_csv('C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//Maria//monitor_calibration//normalised_green.csv')
+norm_blue.to_csv('C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//Maria//monitor_calibration//normalised_blue.csv')
+
+average_red=[]
+
+for i in norm_red:
+    average_red.append
+    
+    
+
+
+#for i in norm_red:
+    
