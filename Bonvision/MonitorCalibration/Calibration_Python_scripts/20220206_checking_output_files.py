4b5561a1f6e91842bf1fe88f332be1ba7b3415d1
Maria Cozan <maria.cozan@gmail.com>
2022-02-06 12:35:38 +0000

    script for checking output

--- Bonvision/MonitorCalibration/Calibration_Python_scripts/20220206_checking_output_files.py
+++ Bonvision/MonitorCalibration/Calibration_Python_scripts/20220206_checking_output_files.py
@@ -7,12 +7,14 @@ Created on Sun Feb  6 12:24:06 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_path_b= "C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//MonitorCalibration//output_files//beforeC//Calibration_blue1"
-file_path_g= "C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//MonitorCalibration//output_files//beforeC//Calibration_green1"
-file_path_r= "C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//MonitorCalibration//output_files//beforeC//Calibration_red1"
+file_path_b= "C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//MonitorCalibration//output_files//different_brightness//Calibration_blue1"
+file_path_g= "C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//MonitorCalibration//output_files//different_brightness//Calibration_green1"
+file_path_r= "C://Users//maria//Documents//GitHub//ExperimentalProtocols//Bonvision//MonitorCalibration//output_files//different_brightness//Calibration_red1"
 
 array_b= np.fromfile(file_path_b,dtype='float64')
 array_g= np.fromfile(file_path_g,dtype='float64')
 array_r= np.fromfile(file_path_r,dtype='float64')
 
-plt.plot(array_b, array_g, array_r)
+plt.plot(array_b, color="blue") 
+plt.plot(array_g, color="green") 
+plt.plot(array_r, color="red")
